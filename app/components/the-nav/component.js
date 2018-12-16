28280a77644d02039022ffa53ea15278ed3087a3
magicrobots <adam@magicrobots.com>
2018-12-15 22:01:09 -0500

    added scss, started creating layout

--- app/components/the-nav/component.js
+++ app/components/the-nav/component.js
@@ -3,10 +3,10 @@ import Component from '@ember/component';
 export default Component.extend({
     classNames: ['nav--left'],
 
-    navButtons: [
+    navButtons: Object.freeze([
         {title: 'abooot', route: 'about'},
         {title: 'images', route: 'images'},
         {title: 'help', route: 'help'},
         {title: 'search', route: null}
-    ]
+    ])
 });
