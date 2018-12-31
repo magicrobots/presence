3ff37dc2b3eadd11eebab944e45a275e2cd06363
magicrobots <adam@magicrobots.com>
2018-12-31 14:55:54 -0500

    added some images and stuff

--- app/components/route-container/component.js
+++ app/components/route-container/component.js
@@ -6,30 +6,29 @@ import ResizeObservable from 'ember-resize-observer/mixins/resize-observable';
 export default Component.extend(ResizeObservable, {
 
     didRender() {
-        this._setCOntainerSize();
+        this._setContainerSize();
     },
 
     observedResize() {
-        this._setCOntainerSize();
+        this._setContainerSize();
     },
 
-    _setCOntainerSize() {
+    _setContainerSize() {
         set(this, 'containerHeight', window.innerHeight);
         set(this, 'containerWidth', window.innerWidth);
+        this._doBackgroundCycle();
     },
 
     routeContainerStyle: computed('containerHeight', 'containerWidth', {
         get() {
             // make it a 4:3 ratio as big as possible in the viewport
-            // minus footer height
 
             const border = 50;
-            const lilBuffer = 10;
             const outputRatio = 4/3;
             const currHeight = this.containerHeight;
             const currWidth = this.containerWidth;
-            const maxHeight = currHeight - (border * 2) - lilBuffer;
-            const maxWidth = currWidth;
+            const maxHeight = currHeight - (border * 2);
+            const maxWidth = currWidth - (border * 2);
             const isWideViewport = maxWidth / maxHeight > outputRatio;
             
             let newHeight;
@@ -40,14 +39,14 @@ export default Component.extend(ResizeObservable, {
             if (isWideViewport) {
                 newHeight = maxHeight;
                 newWidth = outputRatio * newHeight;
-                newLeft = (currWidth - newWidth) / 2;
                 newTop = 0;
             } else {
                 newWidth = maxWidth;
                 newHeight = maxWidth * (1 / outputRatio);
-                newLeft = 0;
                 newTop = (currHeight - newHeight) / 2 - (border * 1);
             }
+
+            newLeft = (currWidth - newWidth) / 2;
 
             const styleString = `height: ${newHeight}px; 
                 width: ${newWidth}px; 
@@ -56,5 +55,20 @@ export default Component.extend(ResizeObservable, {
 
             return htmlSafe(styleString);
         }
-    })
+    }),
+
+    _doBackgroundCycle() {
+        const imgContainer = this.$('.route-container');
+        let bgCounter = 1;
+
+        const doSnow = setInterval(function() {
+              imgContainer.css('background-image', `url("assets/snow-${bgCounter}.jpg")`);
+              bgCounter++;
+
+              if(bgCounter > 5) {
+                  clearInterval(doSnow);
+              imgContainer.css('background-image', 'url("assets/emptyScreen.jpg")');
+              }
+        }, 125);
+    }
 });
