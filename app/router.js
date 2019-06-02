bfa6eb9f0c2d8bdd6ebd05103bf0ef7bfe5262df
magicrobots <adam@magicrobots.com>
2019-06-02 12:32:11 -0400

    added new images, deleted old images, made route list smart

--- app/router.js
+++ app/router.js
@@ -1,27 +1,20 @@
 import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
+import commandRegistry from './const/command-registry';
 
 const Router = EmberRouter.extend({
   location: config.locationType,
   rootURL: config.rootURL
 });
 
-// about images help | search?
 Router.map(function() {
-  this.route('cmd-about');
-  this.route('cmd-beep');
-  this.route('cmd-cd');
-  this.route('cmd-clear');
-  this.route('cmd-contact');
-  this.route('cmd-hello');
-  this.route('cmd-man');
-  this.route('cmd-ls');
-  this.route('cmd-pwd');
-  this.route('cmd-stills');
-  this.route('cmd-settings');
-  this.route('cmd-shop');
-  this.route('cmd-story');
-  this.route('cmd-whoami');
+
+  const uniqueRoutnames = [...new Set(commandRegistry.registry.map(cmd => cmd.routeName))];
+
+  const scope = this;
+  uniqueRoutnames.forEach((currRouteName) => {
+    scope.route(currRouteName);
+  });
 });
 
 export default Router;
