e51aae2d578ede1d07481b5efd1eae5c2e50866d
magicrobots <adam@magicrobots.com>
2019-06-11 10:29:00 -0400

    fixing onclick issue, adding safari feedback, preventing ESC minimizing window

--- app/services/platform-analyzer.js
+++ app/services/platform-analyzer.js
@@ -3,5 +3,9 @@ import Service from '@ember/service';
 export default Service.extend({
     getIsSafari() {
         return navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1;
+    },
+    
+    getIsMobileDevice() {
+        return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
     }
 });
