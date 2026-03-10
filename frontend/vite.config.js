0b2e865363c176fd90225bc329bdefe7f0f2305d
Adam Hilliker <adam@magicrobots.com>
2026-03-10 14:33:41 -0400

    responsive(crt-display): [003] fix publicDir path and outlet-container z-index

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
-    publicDir: 'public',
+    publicDir: '../public',
     build: {
         outDir: 'dist'
     },
