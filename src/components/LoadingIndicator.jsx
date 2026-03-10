be3e6b87f75a6eab9a6ed08000d3a90ba2486f3c
Adam Hilliker <adam@magicrobots.com>
2026-03-10 15:07:03 -0400

    chore: update .gitignore for monorepo workspaces, untrack dist/ and orphaned src/

--- src/components/LoadingIndicator.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function LoadingIndicator({ isVisible }) {
-    if (!isVisible) return null;
-
-    return (
-        <img className="loading-indicator" src="assets/loadingIndicator.gif" alt="loading" />
-    );
-}
