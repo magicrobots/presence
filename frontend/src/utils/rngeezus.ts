f431071bd25cecd9b569ec5fdd02b8d7bc4d8720
Adam Hilliker <adam@magicrobots.com>
2026-03-11 15:11:13 -0400

    tech(debt-refactor): [004] rename rngeezus.js to rngeezus.ts with typed exports

--- frontend/src/utils/rngeezus.ts
+++ frontend/src/utils/rngeezus.ts
@@ -1,5 +1,5 @@
 // large value pool
-const largeDisplacementPool = [];
+const largeDisplacementPool: number[] = [];
 const maxDisplacement = 20;
 for (let i = 0; i < 20000; i++) {
     const newValue = Math.ceil(Math.random() * maxDisplacement) - (maxDisplacement / 2);
@@ -7,17 +7,19 @@ for (let i = 0; i < 20000; i++) {
 }
 
 // normal pool
-const smallPool = [];
+const smallPool: number[] = [];
 for (let i = 0; i < 20000; i++) {
     smallPool.push(Math.random());
 }
 
 let rngIndex = 0;
 
+export type PoolName = 'largeDisplacementPool' | 'smallPool';
+
 // Currently the values in initPool are specific to the displacement deformation in iza-computer
 // if you use it anywhere else you'll need to make some updates
-function getRandomValue(poolName) {
-    const pools = { largeDisplacementPool, smallPool };
+function getRandomValue(poolName: PoolName): number {
+    const pools: Record<PoolName, number[]> = { largeDisplacementPool, smallPool };
     const selectedPool = pools[poolName];
     const currIndex = rngIndex;
     const retVal = selectedPool[currIndex];
